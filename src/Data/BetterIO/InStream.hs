d9076888ef6c11e076fdbc268cddd1124d76e230
joshuarahm <joshuarahm>
2013-05-17 21:55:28 -0600

    added changes to instream

--- src/Data/BetterIO/InStream.hs
+++ src/Data/BetterIO/InStream.hs
@@ -1,2 +1,18 @@
 module Data.BetterIO.InStream where
-	
+	import Data.ByteString (ByteString)
+	import Data.Word
+	import System.IO
+	import qualified Data.ByteString as BS
+
+	class InStream a where
+		inStreamRead :: a -> Integer -> IO ByteString
+		inStreamReadByte :: a -> IO Word8
+		inStreamReadSome :: a -> Integer -> IO ByteString
+
+	instance InStream Handle where
+		inStreamReadByte handle = (BS.hGetSome handle 1) >>= return . BS.head
+		-- inStreamRead :: Handle -> Integer -> IO ByteString
+		inStreamReadSome handle = (BS.hGetSome handle) . fromIntegral 
+		-- inStreamRead :: Handle -> Integer -> IO ByteString
+		inStreamRead handle = (BS.hGet handle) . fromIntegral
+
